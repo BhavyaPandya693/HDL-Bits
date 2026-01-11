e2fcc395a600803430e7d9e7d313fb5f961e9d38
Bhavya Pandya <bhavyapandya862@gmail.com>
2026-01-11 22:23:43 +0530

    Add top_module with 8-bit D flip-flops and selection

---
+++ Verilog Language/Modules: Hierarchy/Moduleshift8.v
@@ -0,0 +1,21 @@
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output [7:0] q 
+);
+    wire [7:0]a,b,c;
+    
+    my_dff8 inst1(.clk(clk), .d(d), .q(a));
+    my_dff8 inst2(.clk(clk), .d(a), .q(b));
+    my_dff8 inst3(.clk(clk), .d(b), .q(c));
+    
+    always @(*)begin
+        case(sel)
+        2'b11: q <= c;
+        2'b10: q <= b;
+        2'b01: q <= a;
+        2'b00: q <= d;
+        endcase
+    end
+endmodule
