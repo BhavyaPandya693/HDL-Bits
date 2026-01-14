b3d681e012911b05df3511186c085e79faa17ead
Bhavya Pandya <bhavyapandya862@gmail.com>
2026-01-14 19:47:28 +0530

    Update bitwise operation for subtraction logic

--- Verilog Language/Modules: Hierarchy/moduleaddsub.v
+++ Verilog Language/Modules: Hierarchy/moduleaddsub.v
@@ -7,7 +7,7 @@ module top_module(
     wire [31:0]x;
     wire y;
     
-    assign x = b^sub;
+    assign x = b^{32{sub}};
     
     add16 inst1(.a(a[15:0]), .b(x[15:0]), .cin(sub), .cout(y), .sum(sum[15:0]));
     add16 inst2(.a(a[31:16]), .b(x[31:16]), .cin(y), .cout(), .sum(sum[31:16]));
